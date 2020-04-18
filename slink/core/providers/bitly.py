205f4eb482a41fda2d0b4853da11d750465a2d59
Damir Chanyshev <hairygeek@yandex.com>
2020-04-18 10:59:32 +0500

    chore: handle more bitly errors

--- slink/core/providers/bitly.py
+++ slink/core/providers/bitly.py
@@ -1,9 +1,15 @@
 import requests
 import cerberus
 
-from ...core.errors import WrongResponse, Forbidden
+from ...core.errors import WrongResponse, Forbidden, SLinkError
 from ...core.utils import reraise_requests
 from ..main import Provider
+
+
+class BitlyError(SLinkError):
+    def __init__(self, error_message: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.message = f'Bitly error. Error message :"{error_message}"'
 
 
 class BitlyProvider(Provider):
@@ -26,8 +32,10 @@ class BitlyProvider(Provider):
         resp = requests.post(self._SHORTEN_API_URL, headers=headers, json=data, timeout=5)
         if resp.status_code == 403:
             raise Forbidden
+        if resp.status_code in (400, 402, 403, 404, 417, 422):
+            raise BitlyError(resp.json()['description'])
         json_resp = resp.json()
-        is_valid = cerberus.Validator(self._RESPONSE_SCHEMA, allow_unknown=True).validate(json_resp)
+        is_valid = cerberus.Validator(self._RESPONSE_SCHEMA, allow_unknown=True, require_all=True).validate(json_resp)
         if not is_valid:
             raise WrongResponse
         return json_resp['link']
